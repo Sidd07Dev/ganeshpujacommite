4e81e268794535bdaf212a17daed317305993cc9
Sidhanta Pradhan <sidd07dev@gmail.com>
2024-09-01 19:58:32 +0530

    add new changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="bg-slate-900 text-white py-6">
       <div className="container mx-auto text-center">
         <p>&copy; 2024 Ganesh Puja Committee, Bharathapur. All rights reserved.</p>
-        <p>Designed with ❤️ by the Ganesh Puja Committee Web Team.</p>
+        <p>Designed with ❤️ by Happy.</p>
       </div>
     </footer>
   );
